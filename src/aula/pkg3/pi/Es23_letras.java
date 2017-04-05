c32f690cc0f3460922c626d5a4ec05733b666b50
aline.poliveira3 <aline.poliveira3@CAS555557W0704.senacsp.edu.br>
2017-04-04 22:41:47 -0300

    teste de letras vogal/consoante

--- src/aula/pkg3/pi/Es23_letras.java
+++ src/aula/pkg3/pi/Es23_letras.java
@@ -12,11 +12,38 @@ import java.util.Scanner;
  * @author aline.poliveira3
  */
 public class Es23_letras {
+
     public static void main(String[] args) {
         Scanner NN = new Scanner(System.in);
-        
+
         System.out.print("Digite uma letra: ");
         String letra = NN.nextLine();
-        
+        boolean vogal = false;
+
+        switch (letra.toUpperCase()) {
+            case "A":
+                vogal = true;
+                break;
+            case "E":
+                vogal = true;
+                break;
+            case "I":
+                vogal = true;
+                break;
+            case "O":
+                vogal = true;
+                break;
+            case "U":
+                vogal = true;
+                break;
+            default:
+                vogal = false;
+                break;
+        }
+        if (vogal) {
+            System.out.printf("%S é uma vogal", letra);
+        } else {
+            System.out.printf("%S não é uma vogal", letra);
+        }
     }
 }
